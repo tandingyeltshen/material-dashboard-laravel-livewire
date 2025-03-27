207726b9328a4e4623173756e14c2d3a26c0bf91
Komal Sundas <komalesther506@gmail.com>
2025-03-27 10:13:18 +0600

    changes

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -5,8 +5,8 @@
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href=" {{ route('dashboard') }} ">
-                <img src="{{ asset('assets') }}/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-2 font-weight-bold text-white">Material Dashboard 2 Laravel Livewire</span>
+                <img src="{{ asset('assets') }}/img/logo_bt.png" class="navbar-brand-img h-100" alt="main_logo">
+                <span class="ms-2 font-weight-bold text-white"><b>NOC</b> REPORT SYSTEM </span>
             </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
